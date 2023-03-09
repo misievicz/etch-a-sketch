89945ea1b9a65ec962a3f0d78eab2b93b6f24f2a
misievicz <misievicz@gmail.com>
2023-03-09 06:46:18 +0100

    add grid pattern variable

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const main = document.querySelector('.main-container');
 
-const number = 24;
+let number = 6;
+let gridBasis = 700 / number;
+let gridBasisString = gridBasis.toString();
+gridBasisString += "px";
 
-for (let i = 0; i < 24 * 24; i++) {
+document.documentElement.style.setProperty("--basis", gridBasisString);
+
+for (let i = 0; i < number * number; i++) {
     const grid = document.createElement("div");
     grid.classList.add('grid');
     main.appendChild(grid);
