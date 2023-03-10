afbe8a7f56b787177df467be563b37bc3168ebcd
misievicz <misievicz@gmail.com>
2023-03-10 14:39:30 +0100

    add working solution to grid and synth color

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 const main = document.querySelector('.main-container');
-
-let number = 10;
+let number = 16;
 let gridBasis = 700 / number;
 let gridBasisString = gridBasis.toString();
 gridBasisString += "px";
 document.documentElement.style.setProperty("--basis", gridBasisString);
-
-
 
 function addGrid() {
     let grid = document.createElement("div");
@@ -14,17 +11,20 @@ function addGrid() {
     main.appendChild(grid);
 }
 
-
 for (let i = 0; i < number * number; i++) {
     addGrid();
 }
 
 let cell = main.children; 
-cell[55].style.backgroundColor = "white";
- 
 
-for (i = 0; i < number * number - 1; i++) {
+for (let i = 0; i < number * number; i++) {
     cell[i].addEventListener("mouseover", function () {
-        console.log('hi');
-    });
+        cell[i].style.backgroundColor = red();
+  });
+}
+
+function red() {
+    return (
+      "rgb(" + Math.floor(Math.random() * 256) + "," + 56 + "," + 200 + ")"
+    );
 }
