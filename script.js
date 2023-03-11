5b22ad8dbbde1a5ea9da1ae0bc85ce41ab5cb472
misievicz <misievicz@gmail.com>
2023-03-11 10:38:47 +0100

    add size and mode functions

--- script.js
+++ script.js
@@ -2,28 +2,65 @@ function red() {
   return "rgb(" + Math.floor(Math.random() * 256) + "," + 56 + "," + 220 + ")";
 }
 
+const size = 16;
+function changeSize(size) {
+    return newSize = size;
+}
+
 const main = document.querySelector(".main-container");
 
-let number = 16;
-let gridBasis = 700 / number;
-let gridBasisString = gridBasis.toString();
-gridBasisString += "px";
-document.documentElement.style.setProperty("--basis", gridBasisString);
-
-function addGrid() {
-  let grid = document.createElement("div");
-  grid.classList.add("grid");
-  main.appendChild(grid);
+function makeGrid(number) {
+    let gridBasis = 700 / number;
+    let gridBasisString = gridBasis.toString();
+    gridBasisString += "px";
+    document.documentElement.style.setProperty("--basis", gridBasisString);
+    for (let i = 0; i < number * number; i++) {
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        main.appendChild(grid);
+    }
 }
-
-for (let i = 0; i < number * number; i++) {
-  addGrid();
-}
-
+makeGrid(size);
 let cell = main.children;
 
-for (let i = 0; i < number * number; i++) {
-  cell[i].addEventListener("mouseover", function () {
-    cell[i].style.backgroundColor = red();
-  });
+function reset() {
+    while (main.firstChild) {
+    main.removeChild(main.lastChild);
+    }
 }
+
+
+let b = document.querySelector('button')
+b.addEventListener('click', function (e) {
+    reset();
+    makeGrid(16);
+    changeSize(16);
+});
+
+let b1 = document.getElementById("#btns1");
+b1.addEventListener("click", function (e) {
+    reset();
+    makeGrid(64);
+    changeSize(64);
+});
+
+let b2 = document.getElementById("#btns2");
+b2.addEventListener("click", function (e) {
+    reset();
+    makeGrid(newSize);
+});
+
+let b3 = document.getElementById("#btns3");
+
+b3.addEventListener("click", function (e) {
+    for (let i = 0; i < cell.length; i++) {
+      cell[i].addEventListener("mouseover", function () {
+        cell[i].style.backgroundColor = red();
+      });
+    }
+});
+
+
+
+
+
