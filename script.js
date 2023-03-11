96736b7861704fecf0e8ab2e807291e209ac9740
misievicz <misievicz@gmail.com>
2023-03-11 11:56:58 +0100

    add most funciotnalites

--- script.js
+++ script.js
@@ -2,65 +2,66 @@ function red() {
   return "rgb(" + Math.floor(Math.random() * 256) + "," + 56 + "," + 220 + ")";
 }
 
-const size = 16;
-function changeSize(size) {
-    return newSize = size;
-}
-
 const main = document.querySelector(".main-container");
-
-function makeGrid(number) {
-    let gridBasis = 700 / number;
-    let gridBasisString = gridBasis.toString();
-    gridBasisString += "px";
-    document.documentElement.style.setProperty("--basis", gridBasisString);
-    for (let i = 0; i < number * number; i++) {
-        let grid = document.createElement("div");
-        grid.classList.add("grid");
-        main.appendChild(grid);
-    }
-}
-makeGrid(size);
 let cell = main.children;
 
-function reset() {
-    while (main.firstChild) {
-    main.removeChild(main.lastChild);
-    }
+function changeSize(size) {
+  return (newSize = size);
+}
+function changeMode(color) {
+  return (currentColor = color);
+}
+function makeGrid(number) {
+  let gridBasis = 700 / number;
+  let gridBasisString = gridBasis.toString();
+  gridBasisString += "px";
+  document.documentElement.style.setProperty("--basis", gridBasisString);
+  for (let i = 0; i < number * number; i++) {
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    main.appendChild(grid);
+  }
+}
+function makeColor(color) {
+  for (let i = 0; i < cell.length; i++) {
+    cell[i].addEventListener("mouseover", function () {
+      cell[i].style.backgroundColor = color;
+    });
+  }
+  changeMode(color);
 }
 
+function reset() {
+  while (main.firstChild) {
+    main.removeChild(main.lastChild);
+  }
+}
 
-let b = document.querySelector('button')
-b.addEventListener('click', function (e) {
-    reset();
-    makeGrid(16);
-    changeSize(16);
+document.querySelector("button").addEventListener("click", function (event) {
+  reset();
+  makeGrid(16);
+  changeSize(16);
+  makeColor(currentColor);
 });
 
-let b1 = document.getElementById("#btns1");
-b1.addEventListener("click", function (e) {
-    reset();
-    makeGrid(64);
-    changeSize(64);
+document.getElementById("#btns1").addEventListener("click", function (event) {
+  reset();
+  makeGrid(64);
+  changeSize(64);
+  makeColor(currentColor);
 });
 
 let b2 = document.getElementById("#btns2");
 b2.addEventListener("click", function (e) {
-    reset();
-    makeGrid(newSize);
+  reset();
+  makeGrid(16);
+  makeColor(currentColor);
 });
 
 let b3 = document.getElementById("#btns3");
-
 b3.addEventListener("click", function (e) {
-    for (let i = 0; i < cell.length; i++) {
-      cell[i].addEventListener("mouseover", function () {
-        cell[i].style.backgroundColor = red();
-      });
-    }
+  makeColor("white");
+  changeMode("white");
 });
 
-
-
-
-
+window.onload = makeGrid(16), makeColor("white");
