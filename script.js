6cff9ebd2475d482b06c524534b0a552cd051a69
misievicz <misievicz@gmail.com>
2023-03-11 06:57:29 +0100

    changing grid functions

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-const main = document.querySelector('.main-container');
-let number = 20;
+function red() {
+  return "rgb(" + Math.floor(Math.random() * 256) + "," + 56 + "," + 220 + ")";
+}
+
+const main = document.querySelector(".main-container");
+
+let number = 16;
 let gridBasis = 700 / number;
 let gridBasisString = gridBasis.toString();
 gridBasisString += "px";
 document.documentElement.style.setProperty("--basis", gridBasisString);
 
 function addGrid() {
-    let grid = document.createElement("div");
-    grid.classList.add("grid");
-    main.appendChild(grid);
+  let grid = document.createElement("div");
+  grid.classList.add("grid");
+  main.appendChild(grid);
 }
 
 for (let i = 0; i < number * number; i++) {
-    addGrid();
+  addGrid();
 }
 
-let cell = main.children; 
+let cell = main.children;
 
 for (let i = 0; i < number * number; i++) {
-    cell[i].addEventListener("mouseover", function () {
-        cell[i].style.backgroundColor = red();
+  cell[i].addEventListener("mouseover", function () {
+    cell[i].style.backgroundColor = red();
   });
-}
-
-function red() {
-    return (
-      "rgb(" + Math.floor(Math.random() * 256) + "," + 56 + "," + 220 + ")"
-    );
 }
